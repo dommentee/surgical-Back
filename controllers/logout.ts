6d9135ac5b7d0484922a1c54e1901f2832bbc3c6
dommentee <chuku0929@gmail.com>
2022-03-14 14:55:58 -0400

    chnged backend

--- controllers/logout.ts
+++ controllers/logout.ts
@@ -1,11 +1,11 @@
 import { signedCookies } from 'cookie-parser';
 import express from 'express'
 import { Cookie } from 'express-session';
-// import { clearLoginCookies } from '../models/authentication';
+import { clearLoginCookies } from '../models/authentication';
 const router = express.Router();
 
 router.post('/', (req,res, next) => {
-    // clearLoginCookies(req, res, next);
+    clearLoginCookies(req, res, next);
     res.cookie("refresh-token", "");
     res.cookie("access-token", "");
     res.send('please')
